bfd871400fb55872d7f4be5b7ab4a71867c5762a
xuming liu <xumingliu@epiclouds.nett>
2017-12-26 10:01:25 +0800

    12/26

---
+++ app_workspace/baidu/kuaisan/app/build/generated/source/buildConfig/androidTest/online_360/debug/com/lxm/ss/sscai/test/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lxm.ss.sscai.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lxm.ss.sscai.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "online_360";
+  public static final int VERSION_CODE = 101;
+  public static final String VERSION_NAME = "1.1";
+  // Fields from product flavor: online_360
+  public static final String APPSHOW_ADID = "yj20171208008";
+  public static final boolean IS_DEBUG = true;
+  public static final String MY_SWITCH_REG = "360shishicai=(\\d{1})";
+  public static final String SERVER_PATH = "http://app.fromfactory.club/";
+}
---
+++ app_workspace/baidu/kuaisan/app/build/generated/source/buildConfig/online_baidu/debug/com/lxm/ss/kuaisan/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lxm.ss.kuaisan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lxm.ss.kuaisan";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "online_baidu";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: online_baidu
+  public static final String APPSHOW_ADID = "yj20171120002";
+  public static final boolean IS_DEBUG = true;
+  public static final String MY_SWITCH_REG = "baidukuaisan=(\\d{1})";
+  public static final String SERVER_PATH = "http://app.fromfactory.club/";
+}
